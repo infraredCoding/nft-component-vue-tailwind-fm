fc26710702f4379d5ed0beefcc292a27c7c89b27
Imran Rahman <imranspeedcuber@gmail.com>
2021-12-03 22:25:56 +0600

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? 'nft-component-vue-tailwind-fm/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/nft-component-vue-tailwind-fm/' : '/'
 }
